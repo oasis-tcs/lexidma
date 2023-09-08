0661d0580cab06b6624cc54070ce21ecc86dc928
dF <DavidFatDavidF@users.noreply.github.com>
2023-09-08 11:48:08 +0000

    renaming build files according to OASIS convention
    Changed name files in build files to correspond to the approved csd01 stage

--- dmlex-v1.0/specification/merge.bat
+++ dmlex-v1.0/specification/merge.bat
@@ -1,2 +1,2 @@
-java -cp "lib\merger.jar;lib\resolver.jar;lib\xercesImpl.jar;lib\xml-apis.jar" Merger dmlex.xml dmlex-v1.0-wd01.xml
+java -cp "lib\merger.jar;lib\resolver.jar;lib\xercesImpl.jar;lib\xml-apis.jar" Merger dmlex.xml dmlex-v1.0-csd01.xml
 pause
