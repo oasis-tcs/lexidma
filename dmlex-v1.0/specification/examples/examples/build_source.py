d5cc2b00e96834e84bc83b4df2db42a3d4cb99ff
John P. McCrae <john@mccr.ae>
2024-01-01 14:04:50 +0000

    Also limit XML line length in examples

--- dmlex-v1.0/specification/examples/examples/build_source.py
+++ dmlex-v1.0/specification/examples/examples/build_source.py
@@ -15,7 +15,12 @@ for f in glob("source/*.xml"):
     with open(f) as inf:
         with open(f + ".xml", "w") as outf:
             outf.write("<programlisting>\n")
-            outf.write(inf.read().replace("<", "&lt;").replace(">", "&gt;"))
+            for line in inf.readlines():
+                while len(line) > N:
+                    outf.write(line[:N].replace("<", "&lt;").replace(">", "&gt;"))
+                    outf.write("\n")
+                    line = line[N:]
+                outf.write(line.replace("<", "&lt;").replace(">", "&gt;"))
             outf.write("</programlisting>\n")
 
 for f in glob("source/*.rdf"):
